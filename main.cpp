29ffb94f54971831526caebe6fe526b95c7e5dbc
Martin Wong <martinwong327@gmail.com>
2022-03-03 11:30:07 +0800

    commit code change

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include <unordered_map>
 #include <ctime>
 #include <set>
-#include "ctpl.h"
 
 #define MAX_MARKET 15000
-#define NUM_WORKER_THREAD 2
 #define NUM_OF_PARAM 5
-#define MULTITHREAD true
 
 std::unordered_map<std::string, uint8_t> keyHash = {
     {"market", 1},
@@ -29,7 +26,7 @@ enum MarketInfo {
     TotalCount
 };
 
-void extractInfoFromJson(int id, const std::string &json, double data[MAX_MARKET][NUM_OF_PARAM], std::set<uint16_t> &active_market_set) {
+void extractInfoFromJson(const std::string &json, double data[MAX_MARKET][NUM_OF_PARAM], std::set<uint16_t> &active_market_set) {
     uint16_t market;
     double vol, price;
     bool isBuy;
@@ -86,52 +83,36 @@ void extractInfoFromJson(int id, const std::string &json, double data[MAX_MARKET
 int main() {
     std::string buf;
     std::clock_t start_t = std::clock();
-    double duration, data[NUM_WORKER_THREAD + 1][MAX_MARKET][NUM_OF_PARAM];
+    double duration, data[MAX_MARKET][NUM_OF_PARAM];
     memset(data, 0, sizeof(data));
-    std::set<uint16_t> active_market_set[NUM_WORKER_THREAD + 1];
-    ctpl::thread_pool tp(NUM_WORKER_THREAD);
+    std::set<uint16_t> active_market_set;
 
     int runCount = 0;
     std::cin >> buf;
     if (buf == "BEGIN") {
-        uint8_t instance;
         while (std::cin >> buf) {
             if (buf == "END") break;
             runCount += 1;
 
-            if (MULTITHREAD) {
-                instance = (runCount % NUM_WORKER_THREAD) + 1;
-                tp.push(extractInfoFromJson, buf, data[instance], active_market_set[instance]);
-            } else {
-                extractInfoFromJson(0, buf, data[0], active_market_set[0]);
-            }
+            extractInfoFromJson(buf, data, active_market_set);
 
             if (runCount % 100000 == 0) {
                 std::cout << "runCount: " << runCount << std::endl;
-                duration = ( std::clock() - start_t ) / (double) CLOCKS_PER_SEC;
+                duration = (std::clock() - start_t) / (double) CLOCKS_PER_SEC;
                 std::cout << "Operation took " << duration << " seconds" << std::endl;
             }
         }
     }
 
-    for (int i = 1; i < NUM_WORKER_THREAD + 1; ++i) {
-        active_market_set[0].insert(active_market_set[i].begin(), active_market_set[i].end());
-    }
-
     double totalVol, meanPrice, meanVol, VWAP, percentageBuy, totalCount;
-    for (std::set<uint16_t>::const_iterator it = active_market_set[0].begin(); it != active_market_set[0].end(); ++it) {
-        for (int i = 1; i < NUM_WORKER_THREAD; ++i) {
-            for (int j = 0; j < NUM_OF_PARAM; ++j) {
-                data[0][*it][j] += data[i][*it][j];
-            }
-        }
+    for (std::set<uint16_t>::const_iterator it = active_market_set.begin(); it != active_market_set.end(); ++it) {
 
-        totalCount = data[0][*it][MarketInfo::TotalCount];
-        totalVol = data[0][*it][MarketInfo::TotalVol];
-        meanPrice = data[0][*it][MarketInfo::TotalPrice] / totalCount;
+        totalCount = data[*it][MarketInfo::TotalCount];
+        totalVol = data[*it][MarketInfo::TotalVol];
+        meanPrice = data[*it][MarketInfo::TotalPrice] / totalCount;
         meanVol = totalVol / totalCount;
-        VWAP = data[0][*it][MarketInfo::TotalPriceMultiplyVol] / totalVol;
-        percentageBuy = data[0][*it][MarketInfo::TotalBuy] / totalCount;
+        VWAP = data[*it][MarketInfo::TotalPriceMultiplyVol] / totalVol;
+        percentageBuy = data[*it][MarketInfo::TotalBuy] / totalCount;
         std::cout << "{\"market\":" << *it
             << ",\"total_volume\":" << totalVol
             << ",\"mean_price\":" << meanPrice
